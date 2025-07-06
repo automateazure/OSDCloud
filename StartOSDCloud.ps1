2a2f9941c9792ee31a4d402a60a6665b2075ff58
Brian Brito <bbrito316@yahoo.com>
2025-07-05 23:07:31 -0700

    added version with windows updates
    StartOSDCloud.ps1

---
+++ StartOSDCloud.ps1
@@ -0,0 +1,123 @@
+#region Initialization
+function Write-DarkGrayDate {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [System.String]
+        $Message
+    )
+    if ($Message) {
+        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) $Message"
+    }
+    else {
+        Write-Host -ForegroundColor DarkGray "$((Get-Date).ToString('yyyy-MM-dd-HHmmss')) " -NoNewline
+    }
+}
+function Write-DarkGrayHost {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [System.String]
+        $Message
+    )
+    Write-Host -ForegroundColor DarkGray $Message
+}
+function Write-DarkGrayLine {
+    [CmdletBinding()]
+    param ()
+    Write-Host -ForegroundColor DarkGray '========================================================================='
+}
+function Write-SectionHeader {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [System.String]
+        $Message
+    )
+    Write-DarkGrayLine
+    Write-DarkGrayDate
+    Write-Host -ForegroundColor Cyan $Message
+}
+function Write-SectionSuccess {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [System.String]
+        $Message = 'Success!'
+    )
+    Write-DarkGrayDate
+    Write-Host -ForegroundColor Green $Message
+}
+#endregion
+#region Define Windows OS and Version
+$ScriptName = 'Ensemble_Health_Partners_OSDCloud'
+$ScriptVersion = '25.07.05.1'
+Write-Host -ForegroundColor Green "$ScriptName $ScriptVersion"
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$Product = (Get-MyComputerProduct)
+$Model = (Get-MyComputerModel)
+$Manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
+$OSVersion = 'Windows 11'
+$OSReleaseID = '23H2'
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'en-us'
+#endregion
+
+#region Define Global OSDCloud Variables $Global:MyOSDCloud
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$False
+    WindowsUpdate = [bool]$False
+    WindowsUpdateDrivers = [bool]$False
+    WindowsDefenderUpdate = [bool]$False
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$False
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+}
+
+$DriverPack = Get-OSDCloudDriverPack -Product $Product -OSVersion $OSVersion -OSReleaseID $OSReleaseID
+
+if ($DriverPack){
+    $Global:MyOSDCloud.DriverPackName = $DriverPack.Name
+}
+
+Write-SectionHeader "OSDCloud Variables"
+Write-Output $Global:MyOSDCloud
+
+Write-SectionHeader -Message "Starting OSDCloud"
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+#endre
+gion
+#region Start OSDCloud
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+#endregion
+
+#region Copy unattend.xml to C:\Windows\Panther to skip OOBE
+# Copy unattend.xml to Panther folder
+$UnattendSource = "$PSScriptRoot\\unattend.xml"
+$UnattendTarget = "C:\\Windows\\Panther\\unattend.xml"
+
+if (Test-Path $UnattendSource) {
+    Write-Host "Copying unattend.xml to $UnattendTarget" -ForegroundColor Cyan
+    Copy-Item -Path $UnattendSource -Destination $UnattendTarget -Force
+} else {
+    Write-Host "unattend.xml not found at $UnattendSource" -ForegroundColor Yellow
+}
+
+Write-SectionHeader -Message "OSDCloud Process Complete, Running Custom Actions From Script Before Reboot"
+#endregion
+
+#region End of script and reboot
+# Reboot into Windows
+Write-SectionHeader -Message "Rebooting into Windows in 10 seconds..."
+Start-Sleep 10
+# Restart
+Restart-Computer
+#endregion
